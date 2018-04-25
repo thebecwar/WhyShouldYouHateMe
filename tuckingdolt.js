e6c3d5938a59c9d606b2f78ba27dc106d7723028
Mark Becwar <mark@thebecwar.com>
2018-04-24 21:36:55 -0400

    Have Tucker interview whoever you want

--- tuckingdolt.js
+++ tuckingdolt.js
@@ -1,6 +1,59 @@
 // Main Text Color: #000000
 // Name Text Color: #ffffff
 
+var cropper;
+var cropBounds;
+var interviewImg = null;
+
+function crop() {
+    cropper.destroy();
+    cropper = null;
+    hideImageSelection();
+    processInput();
+}
+
+function showImageSelection() {
+    document.getElementById("imagepicker").style.display = "block";
+    var img = document.getElementById('interviewee');
+    interviewImg = img;
+    img.onload = function() {
+        var img = document.getElementById('interviewee');
+        cropper = new Cropper(img, {
+        aspectRatio: 1.186,
+        crop: function(event) {
+            cropBounds = event.detail;
+            //console.log(event.detail.x);
+            //console.log(event.detail.y);
+            //console.log(event.detail.width);
+            //console.log(event.detail.height);
+            //console.log(event.detail.rotate);
+            //console.log(event.detail.scaleX);
+            //console.log(event.detail.scaleY);
+        }});
+    }
+}
+
+function hideImageSelection() {
+    document.getElementById("imagepicker").style.display = "none";
+}
+
+function onFileSelected(event) {
+  var selectedFile = event.target.files[0];
+  var reader = new FileReader();
+
+  var imgtag = document.getElementById("interviewee");
+  imgtag.title = selectedFile.name;
+
+  reader.onload = function(event) {
+    imgtag.src = event.target.result;
+  };
+
+  reader.readAsDataURL(selectedFile);
+  
+  showImageSelection();
+  
+}
+
 function determineFontHeight(fontStyle) {
   var body = document.getElementsByTagName("body")[0];
   var dummy = document.createElement("div");
@@ -28,7 +81,7 @@ function findFontSize(text, style, context, bounds, max = 80)
     return 20;
 }
 
-function drawTextInBox(text, context, bounds)
+function drawTextInBox(text, context, bounds, arrow=false)
 {
     var size = context.measureText(text);
     size.height = determineFontHeight("font: " + context.font);
@@ -37,6 +90,17 @@ function drawTextInBox(text, context, bounds)
     var y = ((bounds.height - size.height) / 2) + bounds.y;
     
     context.textBaseline = 'hanging';
+    
+    if (arrow)
+    {
+        context.beginPath();
+        
+        context.moveTo(x - 60, bounds.y + 5);
+        context.lineTo(x - 20, bounds.y + (bounds.height / 2))
+        context.lineTo(x - 60, bounds.y + bounds.height - 10);
+        context.closePath();
+        context.fill();        
+    }
     
     // Help position the text by showing the bounding box
     //context.beginPath();
@@ -63,13 +127,25 @@ window.onload = function(){
     var canvas = document.getElementById("scratchCanvas");
     var context = canvas.getContext("2d");
     var imageObj = new Image();
+    var fahbiooo = new Image();
+    fahbiooo.crossOrigin="anonymous";
     imageObj.crossOrigin="anonymous";
     var image = document.getElementById('placeholder');
     imageObj.onload = function(){
+        if (interviewImg != null)
+        {
+            context.drawImage(fahbiooo, cropBounds.x, cropBounds.y, cropBounds.width, cropBounds.height, 1287, 41, 1196, 1008);
+        }
+        else
+        {
+            context.drawImage(fahbiooo, 1287, 41, 1196, 1008);
+        }
         context.drawImage(imageObj, 0, 0, 2554, 1420);
         
         var caption = "geese: dangerous menace";
         var caption2 = "";
+        var name = "FABIO";
+        var occupation = "Some kind of model or something";
 		
         node = document.getElementById('caption');
         if (node.value.length > 0)
@@ -79,6 +155,16 @@ window.onload = function(){
         if (node.value.length > 0)
             caption2 = node.value.toLowerCase();
 		
+        node = document.getElementById('name');
+        if (node.value.length > 0)
+            name = node.value.toUpperCase();
+        
+        node = document.getElementById('occupation');
+        if (node.value.length > 0)
+            occupation = node.value.toUpperCase();
+        
+        name = name + "   |   " + occupation;
+        
 		if (caption2.length > 0)
 		{
 			var bounds = { x: 425, y: 1140, width: 1950, height: 75 };
@@ -97,10 +183,26 @@ window.onload = function(){
 			context.font = "small-caps bold " + size + "pt Sans-Serif";
 			drawTextInBox(caption, context, bounds);
 		}
-		
+
+        
+        
+        var nameBounds = { x: 397, y: 1071, width: 2009, height: 58};
+		var size = findFontSize(name, "", context, nameBounds);
+        context.font = "small-caps bold " + size + "pt Sans-Serif "
+        context.fillStyle = '#f9f3a9'
+        drawTextInBox(name, context, nameBounds, true);
+        
         image.src = canvas.toDataURL();
         context.clearRect(0, 0, canvas.width, canvas.height);
         canvas.remove();
     }
-    imageObj.src = "tuckerfabio.jpg";
+    if (interviewImg != null)
+    {
+        fahbiooo.src = interviewImg.src;
+    }
+    else
+    {
+        fahbiooo.src = "Fahbio.png";
+    }
+    imageObj.src = "TuckYourselfIn.png";
 }
